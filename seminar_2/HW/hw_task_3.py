c446eacbed4cae99b31d17f3747631768ddcda15
MLR <lenar_sova87@mail.ru>
2023-04-11 21:25:48 +0300

    Home work 3 problem, fixed loop for on while to speed up

--- seminar_2/HW/hw_task_3.py
+++ seminar_2/HW/hw_task_3.py
@@ -2,8 +2,12 @@
 # не превосходящие числа N.
 
 n = int(input())
+power = 0
 
-power_of_two = [2 ** i for i in range(n) if 2 ** i <= n]
+power_of_two = []
+while 2 ** power <= n:
+    power_of_two.append(2 ** power)
+    power += 1
 
 for number in power_of_two:
     print(number, end=' ')
