b9ad762ea031553f2694eb8ef3f19069725eb8cc
MLR <lenar_sova87@mail.ru>
2023-04-26 15:06:52 +0300

    Home work 1 problem

---
+++ seminar_6/HW/hw_task_1.py
@@ -0,0 +1,18 @@
+# Заполните массив элементами арифметической прогрессии. 
+# Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+
+"""Функция возвращающая массив заполненый элементами арифметической прогрессии"""
+def arithmetic_progression(a1, d, n):
+    global data_arithmetic_progression
+    data_arithmetic_progression.append(a1)
+    if n - 1: arithmetic_progression(a1 + d, d, n - 1)
+    return m
+
+"""Получение значений и вывод результата"""
+a1 = int(input('Введите значение первого элемента прогрессии : '))
+d = int(input('Введите значение разности прогрессии : '))
+n = int(input('Введите количество элементов прогрессии : '))
+data_arithmetic_progression = []
+arithmetic_progression(a1, d, n)
+print(f'Прогрессия с указанными значениями -> {data_arithmetic_progression}')
